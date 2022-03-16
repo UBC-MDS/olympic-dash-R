1c8963ab1a192689a720d68b97e32b2db2747cd3
Song Yang <syang003@student.ubc.ca>
2022-03-16 15:24:26 -0700

    added dependencies to init

--- init.R
+++ init.R
@@ -9,5 +9,5 @@ options(repos=r)
 # ======================================================================
 
 # packages go here
-install.packages(c('dash', 'readr', 'dplyr','tidyr', 'jsonlite', 'ggplot2', 'ggthemes', 'plotly', 'remotes', 'dashHtmlComponents'))
+install.packages(c('dash', 'readr', 'dplyr','tidyr', 'jsonlite', 'ggplot2', 'ggthemes', 'plotly', 'remotes', 'dashHtmlComponents', 'here'))
 remotes::install_github('facultyai/dash-bootstrap-components@r-release')
